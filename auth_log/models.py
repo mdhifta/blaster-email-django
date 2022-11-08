a73d6661a285a3ab672d2f797814d606875aaadb
M Dhifta <55729354+mdhifta@users.noreply.github.com>
2022-11-08 11:57:13 +0700

    Update models.py

--- auth_log/models.py
+++ auth_log/models.py
@@ -5,4 +5,4 @@ class Users(models.Model):
     fname = models.CharField(max_length=30)
     lname = models.CharField(max_length=30)
     username = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
+    password = models.CharField(max_length=144)
